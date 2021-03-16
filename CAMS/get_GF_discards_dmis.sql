2f41a92e083748ec434a62e7eb1c9f5984c3bd30
ben <drdrumfish@gmail.com>
2021-03-16 16:17:53 -0400

    Create get_GF_discards_dmis.sql

---
+++ CAMS/get_GF_discards_dmis.sql
@@ -0,0 +1,10 @@
+select round(sum(POKGMASS_DISCARD))
+from apsd.dmis_all_years
+where fishing_year = 2019
+
+;
+
+select round(sum(CODGMSS_DISCARD))
+from apsd.dmis_all_years
+where fishing_year = 2019
+
