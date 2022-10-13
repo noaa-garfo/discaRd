7c6e790524f6f24f6b72f36dc659a1bbec399283
galuardi <benjamin.galuardi@noaa.gov>
2022-10-13 16:18:00 -0400

    documentation commit

--- R/discard_herring.R
+++ R/discard_herring.R
@@ -54,12 +54,11 @@ discard_herring <- function(con
 		# Support table import by species
 		
 		# GEAR TABLE
-		CAMS_GEAR_STRATA = tbl(con_maps, sql('  select * from MAPS.CAMS_GEARCODE_STRATA')) %>% 
-			collect() %>% 
-			dplyr::rename(GEARCODE = VTR_GEAR_CODE) %>% 
-			# filter(NESPP3 == species_nespp3) %>% 
-			filter(SPECIES_ITIS == species_itis) %>%
-			dplyr::select(-NESPP3, -SPECIES_ITIS) %>%
+		CAMS_GEAR_STRATA = tbl(con, sql('  select * from CFG_GEARCODE_STRATA')) %>% 
+			collect() %>%
+			dplyr::rename(GEARCODE = VTR_GEAR_CODE) %>%
+			filter(ITIS_TSN == species_itis) %>%  
+			dplyr::select(-NESPP3, -ITIS_TSN) %>% 
 			## AWA change gear group here, ask to have base table changed?
 			#  test <- CAMS_GEAR_STRATA %>%
 			mutate_all(function(x) ifelse(str_detect(x, '^0_'), 'other', x))
@@ -67,71 +66,76 @@ discard_herring <- function(con
 		
 		# Stat areas table  
 		# unique stat areas for stock ID if needed
-		STOCK_AREAS = tbl(con_maps, sql('select * from CAMS_STATAREA_STOCK')) %>%
-			# filter(NESPP3 == species_nespp3) %>%  # removed  & AREA_NAME == species_stock
-			dplyr::filter(SPECIES_ITIS == species_itis) %>%
-			collect() %>% 
-			group_by(AREA_NAME) %>% 
-			distinct(STAT_AREA) %>%
-			mutate(AREA = as.character(STAT_AREA)
-						 , SPECIES_STOCK = AREA_NAME) %>% 
-			ungroup() #%>% 
-		#dplyr::select(SPECIES_STOCK, AREA)
+		STOCK_AREAS = tbl(con, sql('select * from CFG_STATAREA_STOCK')) %>%
+			filter(ITIS_TSN == species_itis) %>%
+			collect() %>%
+			group_by(AREA_NAME, ITIS_TSN) %>%
+			distinct(AREA) %>%
+			mutate(AREA = as.character(AREA)
+						 , SPECIES_STOCK = AREA_NAME) %>%
+			ungroup()
 		
 		# Mortality table
-		CAMS_DISCARD_MORTALITY_STOCK = tbl(con_maps, sql("select * from CAMS_DISCARD_MORTALITY_STOCK"))  %>%
+		CAMS_DISCARD_MORTALITY_STOCK = tbl(con, sql("select * from CFG_DISCARD_MORTALITY_STOCK"))  %>%
 			collect() %>%
 			mutate(SPECIES_STOCK = AREA_NAME
-						 , GEARCODE = CAMS_GEAR_GROUP) %>%
+						 , GEARCODE = CAMS_GEAR_GROUP
+						 , CAMS_GEAR_GROUP = as.character(CAMS_GEAR_GROUP)) %>%
 			select(-AREA_NAME) %>%
-			mutate(CAMS_GEAR_GROUP = as.character(CAMS_GEAR_GROUP)) %>% 
-			# filter(NESPP3 == species_nespp3) %>% 
-			filter(ITIS_TSN == species_itis_srce)
-		# dplyr::select(-NESPP3, -SPECIES_ITIS) %>% 
-		# dplyr::rename(DISC_MORT_RATIO = Discard_Mortality_Ratio)
+			filter(ITIS_TSN == species_itis) %>%
+			dplyr::select(-ITIS_TSN)
 		
 		# Observer codes to be removed
-		OBS_REMOVE = tbl(con_maps, sql("select * from CAMS_OBSERVER_CODES"))  %>%
-			collect() %>% 
-			filter(SPECIES_ITIS == species_itis) %>% 
+		OBS_REMOVE = tbl(con, sql("select * from CFG_OBSERVER_CODES"))  %>%
+			collect() %>%
+			filter(ITIS_TSN == species_itis) %>%
 			distinct(OBS_CODES)
 		
 		#--------------------------------------------------------------------------------#
 		# make tables
-		ddat_focal <- alldat %>% 
-			filter(YEAR == FY) %>%   ## time element is here!!
+		ddat_focal <- all_dat %>% 
+			filter(YEAR == FY) %>%   ## time element is here!!	
 			filter(AREA %in% STOCK_AREAS$AREA) %>% 
 			mutate(LIVE_POUNDS = SUBTRIP_KALL
 						 ,SEADAYS = 0
-						 , NESPP3 = NESPP3_FINAL) %>% 
+						 # , NESPP3 = NESPP3_FINAL
+			) %>% 
 			left_join(., y = STOCK_AREAS, by = 'AREA') %>% 
 			left_join(., y = CAMS_GEAR_STRATA, by = 'GEARCODE') %>% 
 			left_join(., y = CAMS_DISCARD_MORTALITY_STOCK
 								, by = c('SPECIES_STOCK', 'CAMS_GEAR_GROUP')
 			) %>% 
-			dplyr::select(-GEARCODE.y) %>% 
+			dplyr::select(-GEARCODE.y, -NESPP3.y) %>% 
 			dplyr::rename(COMMON_NAME= 'COMMON_NAME.x',SPECIES_ITIS = 'SPECIES_ITIS', NESPP3 = 'NESPP3.x',
 										GEARCODE = 'GEARCODE.x') %>% 
 			relocate('COMMON_NAME','SPECIES_ITIS','NESPP3','SPECIES_STOCK','CAMS_GEAR_GROUP','DISC_MORT_RATIO')
 		
 		
-		ddat_prev <- alldat %>% 
-			filter(YEAR == FY-1) %>%   ## time element is here!!
+		# DATE RANGE FOR PREVIOUS YEAR
+		
+		dr_prev = get_date_range(FY-1, FY_TYPE)
+		end_date_prev = dr_prev[2]
+		start_date_prev = dr_prev[1]
+		
+		
+		ddat_prev <- all_dat %>% 
+			filter(YEAR == FY-1) %>%   ## time element is here!!	
 			filter(AREA %in% STOCK_AREAS$AREA) %>% 
 			mutate(LIVE_POUNDS = SUBTRIP_KALL
 						 ,SEADAYS = 0
-						 , NESPP3 = NESPP3_FINAL) %>% 
+						 # , NESPP3 = NESPP3_FINAL
+			) %>% 
 			left_join(., y = STOCK_AREAS, by = 'AREA') %>% 
 			left_join(., y = CAMS_GEAR_STRATA, by = 'GEARCODE') %>% 
 			left_join(., y = CAMS_DISCARD_MORTALITY_STOCK
 								, by = c('SPECIES_STOCK', 'CAMS_GEAR_GROUP')
 			) %>% 
-			dplyr::select( -GEARCODE.y) %>% 
+			dplyr::select(-NESPP3.y, -GEARCODE.y) %>% 
 			dplyr::rename(COMMON_NAME= 'COMMON_NAME.x',SPECIES_ITIS = 'SPECIES_ITIS', NESPP3 = 'NESPP3.x',
 										GEARCODE = 'GEARCODE.x') %>% 
 			relocate('COMMON_NAME','SPECIES_ITIS','NESPP3','SPECIES_STOCK','CAMS_GEAR_GROUP','DISC_MORT_RATIO')
 		
-		
+
 		
 		# need to slice the first record for each observed trip.. these trips are multi rowed while unobs trips are single row.. 
 		ddat_focal_cy = ddat_focal %>% 
