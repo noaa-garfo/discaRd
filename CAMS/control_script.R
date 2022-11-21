89578fe354327f2a20f03d03c91077abeaa65720
Daniel Hocking <daniel.hocking@noaa.gov>
2022-11-21 13:35:20 -0500

    fixed connection references so can run as a function or in the background

--- CAMS/control_script.R
+++ CAMS/control_script.R
@@ -19,7 +19,9 @@ options(scipen = 999)
 # devtools::load_all()
 
 # unlock keyring
-keyring::keyring_unlock("apsd_ma")
+config_run <- readRDS(here::here("configRun.Rds"))
+pw <- config_run$pw
+keyring::keyring_unlock(keyring = 'apsd_ma', password = pw)
 
 # connect to MAPS
 con_maps = apsdFuns::roracle_login(key_name = 'apsd_ma', key_service = 'maps')
@@ -36,8 +38,8 @@ fy_end = 2022
 ## ----refresh and rebuild obdbs and cams_obs_catch---------------------------------------------------------------------------------
 
 for(i in 2022){
-	require(glue)
-	make_cams_obdbs(con_maps, i, sql_file = system.file("SQL", "make_obdbs_table_cams.sql", package="discaRd"))
+  require(glue)
+  make_cams_obdbs(con_maps, i, sql_file = system.file("SQL", "make_obdbs_table_cams.sql", package="discaRd"))
 }
 
 # CAMS_OBS_CATCH
@@ -68,26 +70,26 @@ species <- tbl(con_maps, sql("
   select *
   from CFG_DISCARD_RUNID
   ")) %>%
-	filter(RUN_ID == 'GROUNDFISH') %>%
-	collect() %>%
-	group_by(ITIS_TSN) %>%
-	slice(1) %>%
-	ungroup()
+  filter(RUN_ID == 'GROUNDFISH') %>%
+  collect() %>%
+  group_by(ITIS_TSN) %>%
+  slice(1) %>%
+  ungroup()
 
 save_dir = file.path(getOption("maps.discardsPath"), 'groundfish')
 
 # run it
 for(fy in fy_start:fy_end){ # TODO: move years to configDefaultRun.toml
-	# FY <- jj
-	# FY_TYPE = 'MAY START' # moved into function
+  # FY <- jj
+  # FY_TYPE = 'MAY START' # moved into function
   # source('groundfish_loop.R') # move this to R/ and run as function
-		# discard_groundfish(con = con_maps
-		# 									 , species = species #[c(7,11),]
-		# 									 , gf_dat = gf_dat
-		#                    , non_gf_dat = non_gf_dat
-		# 									 , gf_trips_only = F
-		# 									 , save_dir = save_dir
-		# 									 , FY = fy)
+  # discard_groundfish(con = con_maps
+  # 									 , species = species #[c(7,11),]
+  # 									 , gf_dat = gf_dat
+  #                    , non_gf_dat = non_gf_dat
+  # 									 , gf_trips_only = F
+  # 									 , save_dir = save_dir
+  # 									 , FY = fy)
 
   parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy, gf_only = F)
 }
@@ -110,24 +112,24 @@ species <- tbl(con_maps, sql("
   select *
   from CFG_DISCARD_RUNID
   ")) %>%
-	filter(RUN_ID == 'CALENDAR') %>%
-	collect() %>%
-	group_by(ITIS_TSN) %>%
-	slice(1) %>%
-	ungroup()
+  filter(RUN_ID == 'CALENDAR') %>%
+  collect() %>%
+  group_by(ITIS_TSN) %>%
+  slice(1) %>%
+  ungroup()
 
 save_dir = file.path(getOption("maps.discardsPath"), "calendar")
 
 for(fy in fy_start:fy_end){ # TODO: move years to configDefaultRun.toml
-	discard_generic(con = con_maps
-										 , species = species
-										 , FY = fy
-										 , all_dat = all_dat
-										 , save_dir = save_dir
-	)
+  discard_generic(con = con_maps
+                  , species = species
+                  , FY = fy
+                  , all_dat = all_dat
+                  , save_dir = save_dir
+  )
 
-		parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
-	}
+  parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
+}
 
 # commit DB
 
@@ -140,30 +142,30 @@ gc()
 
 
 
- ## ----run may year species RMD as a script, eval = F-------------------------------------------------------------------------------
+## ----run may year species RMD as a script, eval = F-------------------------------------------------------------------------------
 
 species <- tbl(con_maps, sql("
   select *
   from CFG_DISCARD_RUNID
   ")) %>%
-	filter(RUN_ID == 'MAY') %>%
-	collect() %>%
-	group_by(ITIS_TSN) %>%
-	slice(1) %>%
-	ungroup()
+  filter(RUN_ID == 'MAY') %>%
+  collect() %>%
+  group_by(ITIS_TSN) %>%
+  slice(1) %>%
+  ungroup()
 
 save_dir = file.path(getOption("maps.discardsPath"), "may")
 
 
 for(fy in fy_start:fy_end){ # TODO: move years to configDefaultRun.toml
-	discard_generic(con = con_maps
-									 , species = species
-									 , FY = fy
-									 , all_dat = all_dat
-									 , save_dir = save_dir
-	)
+  discard_generic(con = con_maps
+                  , species = species
+                  , FY = fy
+                  , all_dat = all_dat
+                  , save_dir = save_dir
+  )
 
-	parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
+  parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
 }
 
 # commit DB
@@ -174,30 +176,30 @@ ROracle::dbCommit(con_maps)
 gc()
 
 
- ## ----run November year species RMD as a script, eval = F -------------------
+## ----run November year species RMD as a script, eval = F -------------------
 
 species <- tbl(con_maps, sql("
   select *
   from CFG_DISCARD_RUNID
   ")) %>%
-	filter(RUN_ID == 'NOVEMBER') %>%
-	collect() %>%
-	group_by(ITIS_TSN) %>%
-	slice(1) %>%
-	ungroup()
+  filter(RUN_ID == 'NOVEMBER') %>%
+  collect() %>%
+  group_by(ITIS_TSN) %>%
+  slice(1) %>%
+  ungroup()
 
 save_dir = file.path(getOption("maps.discardsPath"), "november")
 
 
 for(fy in fy_start:fy_end){ # TODO: move years to configDefaultRun.toml
-	discard_generic(con = con_maps
-									 , species = species
-									 , FY = fy
-									 , all_dat = all_dat
-									 , save_dir = save_dir
-	)
+  discard_generic(con = con_maps
+                  , species = species
+                  , FY = fy
+                  , all_dat = all_dat
+                  , save_dir = save_dir
+  )
 
-	parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
+  parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
 }
 
 # commit DB
@@ -212,32 +214,32 @@ gc()
 
 
 
- ## ----run march year species RMD as a script, eval = F-----------------------------------------------------------------------------
+## ----run march year species RMD as a script, eval = F-----------------------------------------------------------------------------
 
 
-  species <- tbl(con_maps, sql("
+species <- tbl(con_maps, sql("
   select *
   from CFG_DISCARD_RUNID
   ")) %>%
-  	filter(RUN_ID == 'MARCH') %>%
-  	collect() %>%
-  	group_by(ITIS_TSN) %>%
-  	slice(1) %>%
-  	ungroup()
+  filter(RUN_ID == 'MARCH') %>%
+  collect() %>%
+  group_by(ITIS_TSN) %>%
+  slice(1) %>%
+  ungroup()
 
-  save_dir = file.path(getOption("maps.discardsPath"), "march")
+save_dir = file.path(getOption("maps.discardsPath"), "march")
 
 
-  for(fy in fy_start:fy_end){ # TODO: move years to configDefaultRun.toml
-  	discard_generic(con = con_maps
-  									 , species = species
-  									 , FY = fy
-  									 , all_dat = all_dat
-  									 , save_dir = save_dir
-  	)
+for(fy in fy_start:fy_end){ # TODO: move years to configDefaultRun.toml
+  discard_generic(con = con_maps
+                  , species = species
+                  , FY = fy
+                  , all_dat = all_dat
+                  , save_dir = save_dir
+  )
 
-  	parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
-  }
+  parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
+}
 
 # commit DB
 
@@ -250,69 +252,69 @@ gc()
 
 
 
- ## ----run april year species RMD as a script -----------------------------------------------------------------------------
+## ----run april year species RMD as a script -----------------------------------------------------------------------------
 
 
-  species <- tbl(con_maps, sql("
+species <- tbl(con_maps, sql("
   select *
   from CFG_DISCARD_RUNID
   ")) %>%
-  	filter(RUN_ID == 'APRIL') %>%
-  	collect() %>%
-  	group_by(ITIS_TSN) %>%
-  	slice(1) %>%
-  	ungroup()
+  filter(RUN_ID == 'APRIL') %>%
+  collect() %>%
+  group_by(ITIS_TSN) %>%
+  slice(1) %>%
+  ungroup()
 
-  save_dir = file.path(getOption("maps.discardsPath"), "april")
+save_dir = file.path(getOption("maps.discardsPath"), "april")
 
 
-  for(fy in fy_start:fy_end){ # TODO: move years to configDefaultRun.toml
-  	discard_generic(con = con_maps
-  									 , species = species
-  									 , FY = fy
-  									 , all_dat = all_dat
-  									 , save_dir = save_dir
-  	)
+for(fy in fy_start:fy_end){ # TODO: move years to configDefaultRun.toml
+  discard_generic(con = con_maps
+                  , species = species
+                  , FY = fy
+                  , all_dat = all_dat
+                  , save_dir = save_dir
+  )
 
-  	parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
-  }
+  parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
+}
 
-  # commit DB
+# commit DB
 
-  ROracle::dbCommit(con_maps)
+ROracle::dbCommit(con_maps)
 
 # clean the workspace
 gc()
 
 
 
-  ## ----run Herring RMD as a script ----------------------------------------------------------------------------
+## ----run Herring RMD as a script ----------------------------------------------------------------------------
 
-  # remove previous data pull
+# remove previous data pull
 
-  rm(alldat, non_gf_dat, gf_dat)
+rm(alldat, non_gf_dat, gf_dat)
 
-  # pull herring specific data
-  start_year = fy_start
-  end_year = fy_end
+# pull herring specific data
+start_year = fy_start
+end_year = fy_end
 
-  all_dat = get_catch_obs_herring(con_maps, start_year-1, end_year)
+all_dat = get_catch_obs_herring(con_maps, start_year-1, end_year)
 
 
-  species <- tbl(con_maps, sql("
+species <- tbl(con_maps, sql("
   select *
   from CFG_DISCARD_RUNID
   ")) %>%
-  	filter(RUN_ID == 'HERRING') %>%
-  	collect() %>%
-  	group_by(ITIS_TSN) %>%
-  	slice(1) %>%
-  	ungroup()
+  filter(RUN_ID == 'HERRING') %>%
+  collect() %>%
+  group_by(ITIS_TSN) %>%
+  slice(1) %>%
+  ungroup()
 
-  save_dir = file.path(getOption("maps.discardsPath"), "herring")
+save_dir = file.path(getOption("maps.discardsPath"), "herring")
 
 
-  for(fy in start_year:end_year){ # TODO: move years to configDefaultRun.toml
+for(fy in start_year:end_year){ # TODO: move years to configDefaultRun.toml
   # 	discard_herring(con = con_maps
   # 								, species = species
   # 								, FY = fy
@@ -320,15 +322,15 @@ gc()
   # 								, save_dir = save_dir
   # 	)
 
-  	parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
-  }
+  parse_upload_discard(con = con_maps, filepath = save_dir, FY = fy)
+}
 
-  # commit DB
+# commit DB
 
-  ROracle::dbCommit(con_maps)
+ROracle::dbCommit(con_maps)
 
 # clean the workspace
-  gc()
+gc()
 
 
 
@@ -338,9 +340,9 @@ MAPS::indexAllTables(con_maps, tables = "CAMS_DISCARD_ALL_YEARS")
 
 ## ---- Add comments ----
 
-  ## ---- Push to CAMS_GARDO ----
+## ---- Push to CAMS_GARDO ----
 
-devtools::load_all('~/PROJECTS/MAPS/')
+# devtools::load_all('~/PROJECTS/MAPS/')
 
 con_cams = apsdFuns::roracle_login(key_name = 'apsd_ma', key_service = 'cams_garfo')
 
