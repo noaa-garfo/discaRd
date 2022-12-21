3e47220095e64a4d543733b53e25510143577f4f
galuardi <benjamin.galuardi@noaa.gov>
2022-12-21 15:07:29 -0500

    remove MREM trips from rate calculations in groundfish loop observed MREM trips will still get obs value. unobs trips will likely receive an assumed rate since ther in season pprobably doesn' have enough coverage

--- R/discard_groundfish.R
+++ R/discard_groundfish.R
@@ -186,6 +186,7 @@ discard_groundfish <- function(con
   	dplyr::filter(FISHDISP != '090') %>%
   	dplyr::filter(LINK3_OBS == 1) %>%
   	dplyr::filter(substr(LINK1, 1,3) %!in% OBS_REMOVE$OBS_CODES) %>%
+  	dplyr::filter(EM != 'MREM') %>%
     mutate(DISCARD_PRORATE = DISCARD
            , OBS_AREA = AREA
            , OBS_HAUL_KALL_TRIP = OBS_KALL
@@ -219,6 +220,7 @@ discard_groundfish <- function(con
   	dplyr::filter(FISHDISP != '090') %>%
   	dplyr::filter(LINK3_OBS == 1) %>%
   	dplyr::filter(substr(LINK1, 1,3) %!in% OBS_REMOVE$OBS_CODES) %>%
+  	dplyr::filter(EM != 'MREM') %>%
     mutate(DISCARD_PRORATE = DISCARD
            , OBS_AREA = AREA
            , OBS_HAUL_KALL_TRIP = OBS_KALL
