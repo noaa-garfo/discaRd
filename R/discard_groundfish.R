4e2869bbd6c682f1eeb3189b326ae2336e464f1c
Daniel Hocking <daniel.hocking@noaa.gov>
2022-12-27 22:46:55 -0500

    fixed MREM bug for pre-2020 data

--- R/discard_groundfish.R
+++ R/discard_groundfish.R
@@ -220,7 +220,7 @@ discard_groundfish <- function(con
   	dplyr::filter(FISHDISP != '090') %>%
   	dplyr::filter(LINK3_OBS == 1) %>%
   	dplyr::filter(substr(LINK1, 1,3) %!in% OBS_REMOVE$OBS_CODES) %>%
-  	dplyr::filter(EM != 'MREM') %>%
+  	dplyr::filter(is.na(EM) | EM != 'MREM') %>%
     mutate(DISCARD_PRORATE = DISCARD
            , OBS_AREA = AREA
            , OBS_HAUL_KALL_TRIP = OBS_KALL
