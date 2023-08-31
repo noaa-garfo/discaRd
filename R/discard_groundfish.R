1d34280e61e9a383cce119aa5b9e950f792d6f81
galuardi <benjamin.galuardi@noaa.gov>
2023-08-31 15:06:59 -0400

    added a logical so O trumps R on observed MREM trips

--- R/discard_groundfish.R
+++ R/discard_groundfish.R
@@ -644,7 +644,7 @@ discard_groundfish <- function(con
 
     # change discard_source, strata_used, discard_rate, and discard for allocated groundfish stocks ----
 
-    if(allocated == T){
+    if(allocated == T & DISCARD_SOURCE != 'O'){
 
       mrem_idx = emjoin$EM == 'MREM'
       emjoin$STRATA_USED[mrem_idx] = 'RULE BASED'
